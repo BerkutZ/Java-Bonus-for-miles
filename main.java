2f4c7973486548c7e4a9b92a468f88c050b39119
Ruslan Badertdinov <47637446+BerkutZ@users.noreply.github.com>
2020-05-18 18:11:12 +0500

    Update main.java

--- main.java
+++ main.java
@@ -4,5 +4,5 @@ public class Main {
        int bonus_for_miles = 1;
        int bonus_for_miles_current = ticket_cost/20 * bonus_for_miles;
        System.out.print(bonus_for_miles_current);
-         }
     }
+}
